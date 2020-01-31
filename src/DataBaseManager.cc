c5fb61f34f9eb9b0d16d5cfbae16292ff6c2c743
herrkong <herrkong25@gmail.com>
2020-02-01 04:52:14 +0800

    连接数据库

--- src/DataBaseManager.cc
+++ src/DataBaseManager.cc
@@ -1,11 +1,39 @@
-#include <iostream>
+#include "DataBaseManager.h"
 
+bool DataBaseManager::connect(const string & ip ,const string& username , const string&  passwd , const string& databasename )
+{
+    //初始化mysql
+    m_mysql = mysql_init(NULL);
+    
+    if(!mysql_real_connect(m_mysql,ip.c_str(),username.c_str(),passwd.c_str(),databasename.c_str(),0,NULL,0))
+    {
+        cout << "connect mysql failed !" << endl;
+        return false;
+    }
+    cout << "connect mysql success " << endl;
+    return true;
+}
 
-
+bool DataBaseManager::close()
+{
+    mysql_close(m_mysql);
+}
 
 
 int main()
 {
-    return 0;
+
+    string ip = "localhost";
+    string username = "root" ;
+    string passwd = "040404";
+    string databasename = "darwin";
+
+    DataBaseManager obj ;
+    obj.connect(ip,username,passwd,databasename);
+    return 0 ;
 }
+
+
+
+
 
--- src/DataBaseManager.h
+++ src/DataBaseManager.h
@@ -1,16 +1,43 @@
 #pragma once
+#include <mysql/mysql.h>
+#include <iostream>
 
 //数据库管理类
 //封装mysql的c api 
 
+using namespace std;
 
 class DataBaseManager
 {
 public:
-    DataBaseManager() {}
-    ~DataBaseManager() {}
+    DataBaseManager() 
+    {
+        cout << "DataBaseManager()" <<endl;
+    }
+    ~DataBaseManager() 
+    {
+        close();
+        cout << "~DataBaseManager()" <<endl;
+    }
+
+
+public:
+   //连接数据库
+   bool connect(const string & ip ,const string& username , const string&  passwd , const string& databasename );
+   //关闭数据库连接
+   bool close();
+
 
 private:
+    std::string m_ip;
+    std::string m_username;
+    std::string m_passwd;
+    std::string m_databasename;
+
+private:
+    MYSQL * m_mysql ;
+    MYSQL_RES * m_mysql_res;
+    MYSQL_ROW m_mysql_row;
 
 };
 
